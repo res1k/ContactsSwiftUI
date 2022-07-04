39fbaf8f2a5cab26210c9af8a19f14f711dd2c0b
Andrey Zhivotov <res_a@mail.ru>
2022-07-04 21:17:01 +0300

    Initial Commit

---
+++ ContactsSwiftUI/ContactsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsSwiftUIApp.swift
+//  ContactsSwiftUI
+//
+//  Created by Andrey Zhivotov on 04.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
